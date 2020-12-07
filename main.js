c2a512c8abd7edd678166aeab153ee6a6d4cdb1d
Alexandra <sandra.larrosa88@gmail.com>
2020-12-07 11:26:01 +0100

    finished javascript

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-const buttonForm = document.querySelector('.buttonForm');
+
 const buttonAssessment = document.querySelector('.buttonAssessment');
 const buttonShowBank = document.querySelector('.buttonShowBank')
+const buttonShowDay = document.querySelector('.buttonShowDay')
 
-
-const actionButton = (ev) => {
-    ev.preventDefault();
-}
 
 const showAssessment = () => {
     const rangeAssessment = document.querySelector('#assessment').value;
     alert(`Has valorado con ${rangeAssessment} puntos`)
-    console.log('hola mundo', rangeAssessment);
 }
 
 const showBank = () => {
@@ -20,11 +16,25 @@ const showBank = () => {
     const branch = document.querySelector('#branch').value;
     const dc = document.querySelector('#dc').value;
     const account = document.querySelector('#account').value;
-    alert(`Le informamos que su cuenta bancaria es: ${country}${iban}-${bank}-${branch}-${dc}-${account}`)
+
+    if (iban.length < 2 || bank.length < 4 || branch.length < 4 || dc.length < 2 || account.length < 10) {
+        alert('No has introducido correctamente tu cuenta bancaria')
+    } else {
+        alert(`Le informamos que su cuenta bancaria es: ${country}${iban}-${bank}-${branch}-${dc}-${account}`)
+    }
+}
+
+const showDay = () => {
+    const dateForm = new Date(document.querySelector('#date').value);
+    const day = dateForm.getDay();
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'
+    ]
+    const nameDay = days[day]
+    alert(`La fecha seleccionada en el elemento de fecha es ${nameDay}`);
 }
 
 
-buttonForm.addEventListener('click', actionButton)
 buttonAssessment.addEventListener('click', showAssessment)
 buttonShowBank.addEventListener('click', showBank)
+buttonShowDay.addEventListener('click', showDay)
 
